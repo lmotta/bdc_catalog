6c5914bfd8f40c0efdcb655c8499178bc9c18b05
lmotta <motta.luiz@gmail.com>
2025-10-11 18:57:50 -0300

    Added create_zip_plugin.sh

---
+++ create_zip_plugin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+plugin_dir=$( basename $( pwd ) )
+# Remove exists file and directory plugi
+if [ -f "./$plugin_dir.zip" ]; then
+  rm "./$plugin_dir.zip"
+fi
+if [ -d "./$plugin_dir" ]; then
+  rm -r "./$plugin_dir"
+fi
+# Create Plugin Directory
+mkdir "./$plugin_dir"
+# Copy files
+for item in __init__.py metadata.txt LICENSE; do cp "./$item" "./$plugin_dir"; done
+cp -r ./bdc "./$plugin_dir"
+rm -r "./$plugin_dir/bdc/__pycache__"
+cp -r ./resources "./$plugin_dir"
+# Create Translate files
+mkdir "./$plugin_dir/i18n"
+cp ./i18n/*.qm "./$plugin_dir/i18n"
+# Create Zip and remove Plugin Directory
+zip -q -r "$plugin_dir.zip" "./$plugin_dir"
+rm -r "./$plugin_dir"
+#
+echo "Zip file created: "$plugin_dir".zip"
